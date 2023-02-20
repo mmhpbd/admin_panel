365183663a2e8aaecfa4b2d8a8111a0b744df035
mmhpbd <mmhparvez111@gmail.com>
2023-02-21 05:21:37 +0600

    first commit

---
+++ admin_login.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <h1>now start</h1>
+    </body>
+</html>g
