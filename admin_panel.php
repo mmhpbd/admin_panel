472ab67768045535cb3b73255832c9edb4a35668
mmhpbd <mmhparvez111@gmail.com>
2023-02-23 05:06:02 +0600

    php admin panel commit

--- admin_panel.php
+++ admin_panel.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
 ?>
 <html lang="en">
 
@@ -16,12 +17,12 @@
 
 <body>
     <section>
-        <nav class="navbar">
+        <nav class="navbar header">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
                     <img src="./ZEDX EYE.png" alt="Bootstrap" width="150">
                 </a>
-                <a href="#"><i class="fa-solid fa-right-from-bracket"></i></a>
+                <button name="logout"><i class="fa-solid fa-right-from-bracket"></i></button>
             </div>
         </nav>
     </section>
@@ -62,7 +63,6 @@
             </ul>
         </div>
         <div class="partofdesh">
-            <h1>Welcome to ZEDX EYE ADMIN PANEL</h1>
             <div class="row">
                 <div class="col-lg-3">
                     <div class="col-lg-3">
@@ -118,6 +118,18 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
+
+
+
+<?php
+    if(isset($_POST['logout']))
+    {
+        session_destroy();
+        navber("localtion: admin_login.php");
+    }
+?>
+
+
 </body>
 
 </html>
